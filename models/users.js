ceffdea8c2a847f706a8765360c9f8d84709153f
Tamás Gavrán <b0gavtam@vasvari.org>
2024-11-20 15:22:08 +0100

    nekem ennyi elég lesz

--- models/users.js
+++ models/users.js
@@ -1,10 +1,18 @@
 import fs from "fs";
 import __dirname from "../util/rootpath.js"
 
+
+const t = []
 class User{
     constructor(id, name){
         this.id = id;
         this.name = name;
+    }
+    save(){
+        t.push(this)
+    }
+    static getAllUsers(){
+        return t;
     }
 }
 export default User
